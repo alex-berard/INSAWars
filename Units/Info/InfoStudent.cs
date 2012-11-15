14e045a315747249b06d9906c8a2f19d2ba05945
gmalkas <gabriel.malkas@gmail.com>
2012-11-15 09:43:51 +0100

    Add Info units

---
+++ Units/Info/InfoStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INSAWars.Units.EII
+{
+    class InfoStudent : Student
+    {
+        public virtual double AttackPoints { get { return 4.0; } }
+        public virtual double DefensePoints { get { return 2.0; } }
+        public virtual double MovementPoints { get { return 2.0; } }
+        public virtual double HitPoints { get { return 10.0; } }
+    }
+}
---
+++ Units/Info/InfoTeacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INSAWars.Units.EII
+{
+    class InfoTeacher : Teacher
+    {
+        public virtual double DefensePoints { get { return 1.0; } }
+        public virtual double MovementPoints { get { return 3.0; } }
+        public virtual double HitPoints { get { return 1.0; } }
+    }
+}
