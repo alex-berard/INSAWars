5a705c1fb4d2fa50072a1d1ab4f65826a7f2a811
gmalkas <gabriel.malkas@gmail.com>
2012-11-15 09:44:27 +0100

    Add Head class

---
+++ Units/Head.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace INSAWars.Units
+{
+    class Head : Unit
+    {
+        public virtual double DefensePoints { get { return 2.0; } }
+        public virtual double MovementPoints { get { return 3.0; } }
+        public virtual double HitPoints { get { return 5.0; } }
+        public virtual double IronCost { get { return 200.0; } }
+    }
+}
