bdbf80e3a04efa3da3ee1503576a4f4c0b6a0554
gmalkas <gabriel.malkas@gmail.com>
2013-01-02 15:35:58 +0100

    Add UI project

---
+++ UI/NumberOfPlayersPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for NumberOfPlayerPage.xaml
+    /// </summary>
+    public partial class NumberOfPlayersPage : Page
+    {
+        public NumberOfPlayersPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BackButtonClick(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void NextButtonClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
