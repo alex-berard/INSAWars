f99c1250c3009b1e48703d022619b7adbe374495
Alexandre Bérard <eskaev@gmail.com>
2013-01-02 15:47:05 +0100

    DLL fights

--- Game/Game.cs
+++ Game/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using INSAWars.Units;
+using INSAWars.Utils;
 
 namespace INSAWars.Game
 {
--- Utils/InfiniteQueue.cs
+++ Utils/InfiniteQueue.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace INSAWars.Game
+namespace INSAWars.Utils
 {
     class InfiniteQueue<T>
     {
