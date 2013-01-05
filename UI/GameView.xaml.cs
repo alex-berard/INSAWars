1e166dc7c4d8cc6e9a3a075dcfc5f54c53afedb8
gmalkas <gabriel.malkas@gmail.com>
2013-01-05 16:53:01 +0100

    Implements keyboard movement and dramatically reduce memory usage

--- UI/GameView.xaml.cs
+++ UI/GameView.xaml.cs
@@ -28,10 +28,20 @@ namespace UI
         private const int CaseWidth = 96;
         private const int CaseHeight = 96;
 
-        private readonly Tuple<int, int> FoodOffset = Tuple.Create(8, 8);
+        private const int VisibleWidth = 960;
+        private const int VisibleHeight = 768;
+
+        private const int ShiftOffset = 96;
+        private int OffsetX = 0;
+        private int OffsetY = 0;
+
+        private int CaseNumberX;
+        private int CaseNumberY;
+
+        private readonly Tuple<int, int> FoodOffset = new Tuple<int, int>(8, 8);
         private const string FoodTexture = "FoodSmall";
 
-        private readonly Tuple<int, int> IronOffset = Tuple.Create(8, 24);
+        private readonly Tuple<int, int> IronOffset = new Tuple<int, int>(8, 24);
         private const string IronTexture = "IronSmall";
 
         public GameView()
@@ -49,7 +59,20 @@ namespace UI
 
         protected Tuple<int, int> CaseOffset(Case c)
         {
-            return Tuple.Create(c.X * CaseWidth, c.Y * CaseHeight);
+            return new Tuple<int, int>(c.X * CaseWidth, c.Y * CaseHeight);
+        }
+
+        /// <summary>
+        /// Computes an origin for the drawing, since we don't have to render every case. This method
+        /// determines what's the first case we have to draw.
+        /// </summary>
+        /// <param name="offsetX"></param>
+        /// <param name="offsetY"></param>
+        /// <returns></returns>
+        protected Tuple<int, int> DrawingOrigin(int offsetX, int offsetY)
+        {
+            return new Tuple<int, int>(offsetX / CaseWidth,
+                                       offsetY / CaseHeight);
         }
 
         protected BitmapSource DrawMapToBitmap()
@@ -58,30 +81,39 @@ namespace UI
             {
                 Height = Map.Size * CaseHeight;
                 Width = Map.Size * CaseWidth;
-                
-                var drawingVisual = new DrawingVisual();
-                using (var drawingContext = drawingVisual.RenderOpen())
-                {
-                    var rect = new Rect(0, 0, CaseWidth, CaseHeight);
-                    for (int i = 0; i < Map.Size; i++)
-                    {
-                        for (int j = 0; j < Map.Size; j++)
-                        {
-                            rect.X = i * CaseWidth;
-                            rect.Y = j * CaseHeight;
-                            var texture = (BitmapImage)FindResource(Map.GetCaseAt(i, j).Texture);
-                            drawingContext.DrawImage(texture, rect);
-                        }
-                    }
-                }
 
-                var bitmap = new RenderTargetBitmap(
-                    (int)Width, (int)Height, 96, 96, PixelFormats.Default);
-                bitmap.Render(drawingVisual);
+                CaseNumberX = (int)Math.Ceiling((double)VisibleWidth / CaseWidth);
+                CaseNumberY = (int)Math.Ceiling((double)VisibleHeight / CaseHeight);
 
-                _bitmapMap = bitmap;
                 _isMapDrawn = true;
             }
+                
+            var drawingVisual = new DrawingVisual();
+            using (var drawingContext = drawingVisual.RenderOpen())
+            {
+                var rect = new Rect(0, 0, CaseWidth, CaseHeight);
+                var origin = DrawingOrigin(OffsetX, OffsetY);
+
+                System.Diagnostics.Debug.WriteLine(origin);
+                System.Diagnostics.Debug.WriteLine(CaseNumberX);
+
+                for (int i = 0; i < CaseNumberX; i++)
+                {
+                    for (int j = 0; j < CaseNumberY; j++)
+                    {
+                        rect.X = i * CaseWidth;
+                        rect.Y = j * CaseHeight;
+
+                        drawingContext.DrawImage((BitmapImage)FindResource(Map.GetCaseAt(i + origin.Item1, j + origin.Item2).Texture), rect);
+                    }
+                }
+            }
+
+            var bitmap = new RenderTargetBitmap(
+                VisibleWidth, VisibleHeight, 96, 96, PixelFormats.Default);
+            bitmap.Render(drawingVisual);
+
+            _bitmapMap = bitmap;
 
             return _bitmapMap;
         }
@@ -91,7 +123,6 @@ namespace UI
             DrawFood(c.Food, context, CaseOffset(c));
             DrawIron(c.Iron, context, CaseOffset(c));
         }
-
         protected void DrawFood(int amount, DrawingContext context, Tuple<int, int> origin)
         {
             if (amount > 0)
@@ -110,7 +141,6 @@ namespace UI
                 context.DrawText(formattedText, new Point(origin.Item1 + FoodOffset.Item1 + 20, origin.Item2 + FoodOffset.Item2 + 2));
             }
         }
-
         protected void DrawIron(int amount, DrawingContext context, Tuple<int, int> origin)
         {
             if (amount > 0)
@@ -119,14 +149,14 @@ namespace UI
                 context.DrawImage(texture, new Rect(origin.Item1 + IronOffset.Item1,
                                                     origin.Item2 + IronOffset.Item2,
                                                     16, 16));
-                var formattedText = new FormattedText(
+                /*var formattedText = new FormattedText(
                     amount.ToString(),
                     CultureInfo.GetCultureInfo("en-us"),
                     FlowDirection.LeftToRight,
                     new Typeface("Charlemagne STD"),
                     12,
                     Brushes.Red);
-                context.DrawText(formattedText, new Point(origin.Item1 + IronOffset.Item1 + 20, origin.Item2 + IronOffset.Item2 + 2));
+                context.DrawText(formattedText, new Point(origin.Item1 + IronOffset.Item1 + 20, origin.Item2 + IronOffset.Item2 + 2));*/
             }
         }
 
@@ -135,24 +165,25 @@ namespace UI
             base.OnRender(context);
 
             // Draw textures
+            // This takes a lot of memory (~750 Mb) but not much time
             var background = DrawMapToBitmap();
-            context.DrawImage(background, new Rect(0, 0, Width, Height));
+            context.DrawImage(background, new Rect(0, 0, VisibleWidth, VisibleHeight));
 
             // Draw cases' content
-            for (int i = 0; i < Map.Size; i++)
+            // This takes a lot of time, but not much memory
+            /*for (int i = 0; i < Map.Size; i++)
             {
                 for (int j = 0; j < Map.Size; j++)
                 {
                     DrawCaseContent(Map.GetCaseAt(i, j), context);
                 }
-            }
+            }*/
         }
 
         protected Case CaseAtPosition(double x, double y)
         {
-            int i = (int) Math.Floor(x / CaseWidth);
-            int j = (int) Math.Floor(y / CaseHeight);
-            System.Diagnostics.Debug.WriteLine("Case:" + i.ToString() + "," + j.ToString());
+            int i = (int) (x / CaseWidth);
+            int j = (int) (y / CaseHeight);
             return Map.GetCaseAt(i, j);
         }
 
@@ -160,11 +191,46 @@ namespace UI
         {
             double x = e.GetPosition(this).X;
             double y = e.GetPosition(this).Y;
-            System.Diagnostics.Debug.WriteLine("Clic:" + x.ToString() + "," + y.ToString());
-            Case c = CaseAtPosition(x, y);
-            System.Diagnostics.Debug.WriteLine("New Value:" + CaseAtPosition(x, y).Food.ToString());
-            c.Food = 458;
-            this.InvalidateVisual();
+            Case c = CaseAtPosition(x + OffsetX, y + OffsetY);
+            System.Diagnostics.Debug.WriteLine("Case:" + c.X + "," + c.Y);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Left:
+                    if (OffsetX > 0)
+                    {
+                        OffsetX -= ShiftOffset;
+                        this.InvalidateVisual();
+                    }
+                    break;
+                case Key.Right:
+                    
+                    if (OffsetX + (CaseNumberX * CaseWidth) <= Width - CaseWidth)
+                    {
+                        OffsetX += ShiftOffset;
+                        this.InvalidateVisual();
+                    }
+                    break;
+                case Key.Up:
+                    if (OffsetY > 0)
+                    {
+                        OffsetY -= ShiftOffset;
+                        this.InvalidateVisual();
+                    }
+                    break;
+                case Key.Down:
+                    if (OffsetY + (CaseNumberY * CaseHeight) <= Height - CaseHeight)
+                    {
+                        OffsetY += ShiftOffset;
+                        this.InvalidateVisual();
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
