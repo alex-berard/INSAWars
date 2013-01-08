fac9fccb99b7b675cd79ec5ede4e5211d68c5d8c
gmalkas <gabriel.malkas@gmail.com>
2013-01-08 11:48:35 +0100

    Add GameOver window

---
+++ UI/GameOverWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for GameOverWindow.xaml
+    /// </summary>
+    public partial class GameOverWindow : Window
+    {
+        public GameOverWindow(string winnerName)
+        {
+            InitializeComponent();
+            _winnerAcclaim.Text = winnerName + _winnerAcclaim.Text;
+        }
+
+        private void NewGameButtonClick(object sender, RoutedEventArgs e)
+        {
+            var window = new MainWindow();
+            var gameWindow = Application.Current.MainWindow;
+            Application.Current.MainWindow = window;
+            window.Show();
+            gameWindow.Close();
+            Close();
+        }
+
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+            Close();
+        }
+    }
+}
